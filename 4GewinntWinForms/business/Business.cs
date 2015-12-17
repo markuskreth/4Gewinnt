a8b0841add1798055ca0d1ce7556ecc22893d00f
Markus Kreth <markus.kreth@web.de>
2015-12-17 22:00:00 +0100

    XML Kommentare

--- 4GewinntWinForms/business/Business.cs
+++ 4GewinntWinForms/business/Business.cs
@@ -7,7 +7,7 @@ namespace _4GewinntWinForms.business
 {
     class Business
     {
-        private GameState currentState = GameState.Tie;
+        private GameState _currentState = GameState.Tie;
         private CellState[,] cells;
 
         public Business()
@@ -15,16 +15,36 @@ namespace _4GewinntWinForms.business
             startNewGame();
         }
 
+        /// <summary>
+        /// Startet ein ein neues Spiel, ein laufendes wird verworfen.
+        /// </summary>
+        /// <returns></returns>
         public GameState startNewGame()
         {
             cells = new CellState[7, 6];
-            currentState = GameState.Player1;
-            return currentState;
+            _currentState = GameState.Player1;
+            return _currentState;
         }
 
+        /// <summary>
+        /// Aktueller Spielstatus
+        /// </summary>
+        public GameState CurrentState
+        {
+            get
+            {
+                return _currentState;
+            }
+        }
+
+        /// <summary>
+        /// Spielzug des aktuellen Spielers <see cref="CurrentState" />
+        /// </summary>
+        /// <param name="column">Spalte, in die der Spiele seinen Zug setzt</param>
+        /// <returns></returns>
         public GameState doMove(int column)
         {
-            return currentState;
+            return _currentState;
         }
     }
 }
--- 4GewinntWinForms/business/GameState.cs
+++ 4GewinntWinForms/business/GameState.cs
@@ -7,10 +7,25 @@ namespace _4GewinntWinForms.business
 {
     enum  GameState
     {
+        /// <summary>
+        /// Spieler 1 ist am Zug
+        /// </summary>
         Player1,
+        /// <summary>
+        /// Spieler 2 ist am Zug
+        /// </summary>
         Player2,
+        /// <summary>
+        /// Spiel beendet - unentschieden
+        /// </summary>
         Tie,
+        /// <summary>
+        /// Spiel beendet - Spieler 1 hat gewonnen
+        /// </summary>
         Player1HasWon,
+        /// <summary>
+        /// Spiel beendet - Spieler 2 hat gewonnen
+        /// </summary>
         Player2HasWon
     }
 }
