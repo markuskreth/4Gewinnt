0ea31efc4f40419a9c66881e60aee6f3cf28517f
Markus Kreth <markus.kreth@web.de>
2015-12-19 01:18:23 +0100

    Tests für alle horizontalen und vertikalen Extreme

--- 4GewinntTestProject/GameStateCheckerTest.cs
+++ 4GewinntTestProject/GameStateCheckerTest.cs
@@ -27,17 +27,13 @@ namespace _4GewinntTestProject
 
         #endregion
 
-
-        /// <summary>
-        ///Ein Test für "checkGameEnd"
-        ///</summary>
         [TestMethod()]
         public void PlayerWinsLeftBottomHorizontal()
         {
-            cells.set(0, 0,CellState.Player1);
-            cells.set(1, 0,CellState.Player1);
-            cells.set(2, 0,CellState.Player1);
-            cells.set(3, 0,CellState.Player1);
+            cells.set(0, 0, CellState.Player1);
+            cells.set(1, 0, CellState.Player1);
+            cells.set(2, 0, CellState.Player1);
+            cells.set(3, 0, CellState.Player1);
             GameState _currentState = GameState.Player1;
             int column = 0;
             int row = 0;
@@ -60,16 +56,132 @@ namespace _4GewinntTestProject
 
         }
 
-        /// <summary>
-        ///Ein Test für "checkGameEnd"
-        ///</summary>
+        [TestMethod()]
+        public void PlayerWinsRightBottomHorizontal()
+        {
+            cells.set(2, 0, CellState.Player1);
+            cells.set(3, 0, CellState.Player1);
+            cells.set(4, 0, CellState.Player1);
+            cells.set(5, 0, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+            int column = 2;
+            int row = 0;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 4;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 5;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsLeftTopHorizontal()
+        {
+            cells.set(0, 5, CellState.Player1);
+            cells.set(1, 5, CellState.Player1);
+            cells.set(2, 5, CellState.Player1);
+            cells.set(3, 5, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+            int column = 0;
+            int row = 5;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 1;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 2;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsRightTopHorizontal()
+        {
+            cells.set(2, 5, CellState.Player1);
+            cells.set(3, 5, CellState.Player1);
+            cells.set(4, 5, CellState.Player1);
+            cells.set(5, 5, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+            int column = 2;
+
+            int row = 5;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 4;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 5;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsMiddleHorizontal()
+        {
+            cells.set(1, 3, CellState.Player1);
+            cells.set(2, 3, CellState.Player1);
+            cells.set(3, 3, CellState.Player1);
+            cells.set(4, 3, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+
+            int row = 3;
+            int column = 1;
+
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 2;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            column = 4;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
         [TestMethod()]
         public void PlayerWinsLeftBottomVertical()
         {
-            cells.set(0, 0,CellState.Player1);
-            cells.set(0, 1,CellState.Player1);
-            cells.set(0, 2,CellState.Player1);
-            cells.set(0, 3,CellState.Player1);
+            cells.set(0, 0, CellState.Player1);
+            cells.set(0, 1, CellState.Player1);
+            cells.set(0, 2, CellState.Player1);
+            cells.set(0, 3, CellState.Player1);
             GameState _currentState = GameState.Player1;
             int column = 0;
             int row = 0;
@@ -87,6 +199,125 @@ namespace _4GewinntTestProject
             Assert.AreEqual(expected, actual);
 
             row = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsLeftTopVertical()
+        {
+            cells.set(0, 2, CellState.Player1);
+            cells.set(0, 3, CellState.Player1);
+            cells.set(0, 4, CellState.Player1);
+            cells.set(0, 5, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+            int column = 0;
+            int row = 2;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 4;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 5;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsRightBottomVertical()
+        {
+            cells.set(6, 0, CellState.Player1);
+            cells.set(6, 1, CellState.Player1);
+            cells.set(6, 2, CellState.Player1);
+            cells.set(6, 3, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+
+            int column = 6;
+            int row = 0;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 1;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 2;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsRightTopVertical()
+        {
+            cells.set(6, 2, CellState.Player1);
+            cells.set(6, 3, CellState.Player1);
+            cells.set(6, 4, CellState.Player1);
+            cells.set(6, 5, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+
+            int column = 6;
+            int row = 2;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 4;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 5;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PlayerWinsMiddleVertical()
+        {
+            cells.set(3, 1, CellState.Player1);
+            cells.set(3, 2, CellState.Player1);
+            cells.set(3, 3, CellState.Player1);
+            cells.set(3, 4, CellState.Player1);
+            GameState _currentState = GameState.Player1;
+
+            int column = 3;
+            int row = 1;
+            GameState expected = GameState.Player1HasWon;
+            GameState actual;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 2;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 3;
+            actual = target.checkGameEnd(cells, _currentState, column, row);
+            Assert.AreEqual(expected, actual);
+
+            row = 4;
             actual = target.checkGameEnd(cells, _currentState, column, row);
             Assert.AreEqual(expected, actual);
 
